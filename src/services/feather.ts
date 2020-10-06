2ca0106a6528532c67e96c5870e9e670ebd75e3e
Jacob Nielsen <jnillern@gmail.com>
2020-10-06 09:33:39 +0200

    Back to localhost

--- src/services/feather.ts
+++ src/services/feather.ts
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 import feathers from '@feathersjs/feathers';
 import socketio from '@feathersjs/socketio-client';
 
-const socket = io('http://hf5ecapi:3030', {
+const socket = io('http://localhost:3030', {
   transports: ['websocket'],
   forceNew: true,
 });
