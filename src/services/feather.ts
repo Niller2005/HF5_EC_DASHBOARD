ec1bc3d489adf03d22a67c7b4188571055d7ca9e
Jacob Nielsen <jnillern@gmail.com>
2020-10-06 09:45:23 +0200

    Try docker ip

--- src/services/feather.ts
+++ src/services/feather.ts
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 import feathers from '@feathersjs/feathers';
 import socketio from '@feathersjs/socketio-client';
 
-const socket = io('http://localhost:3030', {
+const socket = io('http://172.17.0.3:3030', {
   transports: ['websocket'],
   forceNew: true,
 });
